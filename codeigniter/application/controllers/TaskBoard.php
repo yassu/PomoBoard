14bc900071b9d84940ff139730658bb64b707f05
yassu <mathyassu@gmail.com>
2017-05-19 18:57:19 +0900

    simple index page like helloworld

---
+++ codeigniter/application/controllers/TaskBoard.php
@@ -0,0 +1,8 @@
+<?php
+class TaskBoard extends CI_Controller {
+
+        public function index()
+        {
+            		$this->load->view('taskboard');
+        }
+}
---
+++ codeigniter/application/views/TaskBoard.php
@@ -0,0 +1,5 @@
+<html>
+<body>
+Welcome to The TaskBoard Pages.
+</body>
+</html>
