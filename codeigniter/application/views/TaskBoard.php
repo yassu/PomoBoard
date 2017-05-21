4a6d90e16a5200b32e532119de907db6b887e462
xxxx <xxxxxxxxxxx@gmail.com>
2017-05-21 11:32:24 +0900

    add title and simple header

--- codeigniter/application/views/TaskBoard.php
+++ codeigniter/application/views/TaskBoard.php
@@ -1,8 +1,29 @@
 <html>
 <head>
 	<title>Welcome to TaskBoard</title>
+	<style type="text/css">
+		h1 {
+			color: #444;
+			background-color: transparent;
+			/* border-bottom: 1px solid #D0D0D0; */
+			font-size: 19px;
+			font-weight: normal;
+			margin: 0 0 14px 0;
+			padding: 14px 15px 10px 15px;
+		}
+
+		div.header {
+			background-color: #00ff00;
+			font-size: 24px;
+		}
+	</style>
 </head>
 <body>
-Welcome to The TaskBoard Pages.
+<div class="header">
+Log in
+</div>
+<h1>
+	Welcome to The TaskBoard Pages.
+</h1>
 </body>
 </html>
