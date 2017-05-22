1e25847e2634e8759a65433249ea4ca6f9935cb2
yassu <mathyassu@gmail.com>
2017-05-22 10:48:19 +0900

    use statics items for home page

--- codeigniter/application/controllers/TaskBoard.php
+++ codeigniter/application/controllers/TaskBoard.php
@@ -4,6 +4,9 @@ class TaskBoard extends CI_Controller {
         public function index()
         {
                 $this->load->helper('url');
+
+                $this->load->view('statics/header');
                 $this->load->view('taskboard');
+                $this->load->view('statics/footer');
         }
 }
---
+++ codeigniter/application/views/statics/header.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+	<title>Welcome to TaskBoard</title>
+	<style type="text/css">
+		h1 {
+			color: #444;
+			background-color: transparent;
+			/* border-bottom: 1px solid #D0D0D0; */
+			font-size: 19px;
+			font-weight: normal;
+			margin: 0 0 14px 0;
+			padding: 14px 15px 10px 15px;
+		}
+
+		.header {
+			background-color: #00ff00;
+		}
+		.header_cell {
+			font-size: 24px;
+			border:  solid silver;
+		}
+	</style>
+</head>
+<body>
+<div class="header">
+	<table>
+		<tr>
+			<td class="header_cell">
+				<a href="<?php echo site_url('/task/explore') ?>"> タスク </a>
+			</td>
+			<td class="header_cell"> Log in </td>
+		</tr>
+	</table>
+</div>
