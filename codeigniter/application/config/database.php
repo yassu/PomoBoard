24681ccb25118cbb45b6ecf7499acf5b77cfde0c
yassu <mathyassu@gmail.com>
2017-05-22 18:13:58 +0900

    settings for connecting database

--- codeigniter/application/config/database.php
+++ codeigniter/application/config/database.php
@@ -76,21 +76,18 @@ $query_builder = TRUE;
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
-	'username' => '',
-	'password' => '',
-	'database' => '',
+	'username' => 'root',
+	'password' => 'password',
+	'database' => 'taskboard',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
-	'pconnect' => FALSE,
+	'pconnect' => TRUE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
 	'cache_on' => FALSE,
 	'cachedir' => '',
 	'char_set' => 'utf8',
 	'dbcollat' => 'utf8_general_ci',
 	'swap_pre' => '',
-	'encrypt' => FALSE,
-	'compress' => FALSE,
-	'stricton' => FALSE,
-	'failover' => array(),
-	'save_queries' => TRUE
+	'autoinit' => TRUE,
+	'stricton' => FALSE
 );
