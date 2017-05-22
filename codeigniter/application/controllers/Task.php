faceace056c13daf804b356cb9dd2be506b197bd
yassu <mathyassu@gmail.com>
2017-05-22 11:07:57 +0900

    task/exploreでファイル分けをする

--- codeigniter/application/controllers/Task.php
+++ codeigniter/application/controllers/Task.php
@@ -4,6 +4,9 @@ class Task extends CI_Controller {
         public function explore()
         {
                 $this->load->helper('url');
+
+                $this->load->view('statics/header');
                 $this->load->view('task/explore');
+                $this->load->view('statics/footer');
         }
 }
