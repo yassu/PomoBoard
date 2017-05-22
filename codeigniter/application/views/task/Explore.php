f4eec809cb308113fa4fd7b0fe190de734f5cbd3
yassu <mathyassu@gmail.com>
2017-05-22 11:45:43 +0900

    NewTask のボタンを追加

--- codeigniter/application/views/task/Explore.php
+++ codeigniter/application/views/task/Explore.php
@@ -2,3 +2,8 @@
 	Welcome to The TaskBoard Pages. <br>
 	This is a list page.
 </h1>
+<br>
+
+<button type="button" name="submit" value="new">
+	<div style="font-size: 19px;"> New Task </divß>
+</button>
