beb52d0d08c7f44257f84ef30658729f822bc5e6
yassu <mathyassu@gmail.com>
2017-05-30 11:09:25 +0900

    add task/create

--- codeigniter/application/controllers/Task.php
+++ codeigniter/application/controllers/Task.php
@@ -12,4 +12,16 @@ class Task extends CI_Controller {
                 $this->load->view('task/explore');
                 $this->load->view('statics/footer');
         }
+
+        public function create()
+        {
+                $this->load->helper('url');
+                $this->load->model('MY_User', 'User');
+
+                $header_data['page_title'] = 'Create | TaskBoard';
+
+                $this->load->view('statics/header', $header_data);
+                $this->load->view('task/create');
+                $this->load->view('statics/footer');
+        }
 }
