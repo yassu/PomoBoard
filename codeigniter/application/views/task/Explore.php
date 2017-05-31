1a3f20a72f9744a9d86020ce0019b8bc5a9d631a
yassu <mathyassu@gmail.com>
2017-05-31 11:46:19 +0900

    add form for Task.Search

--- codeigniter/application/views/task/Explore.php
+++ codeigniter/application/views/task/Explore.php
@@ -2,9 +2,30 @@
 	Welcome to The TaskBoard Pages. <br>
 	This is a list page.
 </h1>
-<br>
 
 <?php $create_task_url = site_url('task/create'); ?>
+
+<?php echo form_open('user/login'); ?>
+<table border="1">
+	<tr>
+		<th colspan="6" algin="center"> Search </th>
+	</tr>
+	<tr>
+		<td> Title </td>
+		<td> <input type="text" id="title" name="title" value="" width="100%" /> </td>
+		<td> Memo </td>
+		<td> <input type="text" id="memo" name="memo" value="" width="100%" /> </td>
+		<td> KeyWord </td>
+		<td> <input type="text" id="keyword" name="keyword" value="" width="100%" /> </td>
+	</tr>
+</table>
+
+<button type="button" name="submit" value="submit">
+	<div style="font-size: 19px;"> Execution </div>
+</button>
+
 <button type="button" name="submit" value="create" onClick="location.href = '<?php echo $create_task_url;?>';">
 	<div style="font-size: 19px;"> New Task </div>
 </button>
+
+</form>
