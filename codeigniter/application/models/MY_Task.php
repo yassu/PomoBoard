7e87ae4fd5eb429dc9a244f6a5be61a9d7288e92
xxxx <xxxxxxxxxxx@gmail.com>
2017-06-03 04:15:53 +0900

    add new lines

--- codeigniter/application/models/MY_Task.php
+++ codeigniter/application/models/MY_Task.php
@@ -11,16 +11,19 @@ class MY_Task extends CI_Model
     private $_updated_date;
     private $_is_deleted;
 
+
     public function __construct()
     {
         parent::__construct();
     }
+
 
     public function insert($user_id, $title, $memo)
     {
         $sql = sprintf("INSERT INTO `Task` (`user_id`, `title`, `memo`) VALUES('%s', '%s', '%s');", $user_id, $title, $memo);
         $this->db->query($sql);
     }
+
 
     public function get_list($user_id, $title, $memo, $keyword)
     {
