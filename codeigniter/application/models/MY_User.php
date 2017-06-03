9748df1b8fa1c22747856d568453577f9551d196
xxxx <xxxxxxxxxxx@gmail.com>
2017-06-03 19:13:45 +0900

    refactoring User.insert

--- codeigniter/application/models/MY_User.php
+++ codeigniter/application/models/MY_User.php
@@ -15,8 +15,11 @@ class MY_user extends CI_Model
 
     public function insert($user_id, $hashed_password)
     {
-        $sql = sprintf("INSERT INTO `User` (`user_id`, `user_hashed_pass`) VALUES('%s', '%s');", $user_id, $hashed_password);
-        $this->db->query($sql);
+        $this->db->insert('User', array(
+                'user_id' => $user_id,
+                'user_hashed_pass' => $hashed_password
+                )
+            );
     }
 
 
