07e2dd095bc5d3cd909065c4456b31d97dbb5e2c
xxxx <xxxxxxxxxxx@gmail.com>
2017-06-04 14:01:23 +0900

    for using migration

---
+++ codeigniter/application/migrations/001_add_is_deleted_to_user.php
@@ -0,0 +1,28 @@
+<?php
+class Migration_Add_is_deleted_to_user extends CI_Migration {
+
+    public function __construct()
+    {   
+        parent::__construct();
+    }
+
+    // アップデート処理
+    public function up()
+    {
+        echo "migration.up";
+        $fields = array(
+            'is_deleted' => array (
+                'type' => 'TINYINT',
+                'constraint' => '1',
+                'default' => 0
+            )   
+        );  
+        $this->dbforge->add_column('User', $fields);
+    }   
+
+    // ロールバック処理
+    public function down()
+    {   
+        $this->dbforge->drop_column('User', 'is_deleted');
+    }
+}
