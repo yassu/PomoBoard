470475d8254106d75049658f81a226bcbd284428
yassu <mathyassu@gmail.com>
2017-06-06 16:08:43 +0900

    to all migration files

---
+++ codeigniter/application/migrations/001_initial_migration.php
@@ -0,0 +1,78 @@
+<?php
+class Migration_Initial_migration extends CI_Migration {
+
+    public function __construct()
+    {   
+        parent::__construct();
+    }
+
+    // アップデート処理
+    public function up()
+    {
+        // Create User Table
+        $this->dbforge->add_field(array(
+            'id' => array(
+                'type' => 'INT',
+                'constraint' => 10,
+                'auto_increment' => TRUE
+            ),
+            'user_id' => array(
+                'type' => 'VARCHAR',
+                'constraint' => 64
+            ),
+            'user_hashed_pass' => array(
+                'type' => 'VARCHAR',
+                'constraint' => 256
+            ),
+            'created_date' => array(
+                'type' => 'DATETIME'
+            ),
+            'updated_date' => array(
+                'type' => 'DATETIME'
+            )
+        ));
+        $this->dbforge->add_key('id', TRUE);
+        $this->dbforge->create_table('User');
+
+        // Create Task Table
+        $this->dbforge->add_field(array(
+            'task_id' => array(
+                'type' => 'INT',
+                'constraint' => 10,
+                'auto_increment' => TRUE
+            ),
+            'user_id' => array(
+                'type' => 'VARCHAR',
+                'constraint' => 64
+            ),
+            'title' => array(
+                'type' => 'TEXT'
+            ),
+            'memo' => array(
+                'type' => 'TEXT'
+            ),
+            'created_date' => array(
+                'type' => 'DATETIME'
+            ),
+            'updated_date' => array(
+                'type' => 'DATETIME'
+            ),
+            'is_deleted' => array (
+                'type' => 'TINYINT',
+                'constraint' => '1',
+                'default' => 0
+            )   
+
+        ));
+        $this->dbforge->add_key('task_id', TRUE);
+        $this->dbforge->create_table('Task');
+    }
+
+
+    // ロールバック処理
+    public function down()
+    {   
+        $this->dbforge->drop_table('User');
+        $this->dbforge->drop_table('Task');
+    }
+}
