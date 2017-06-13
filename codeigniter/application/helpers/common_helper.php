18e67af5e82402426f02d4ee2b0dd52d865a4aa0
yassu <mathyassu@gmail.com>
2017-06-13 14:04:07 +0900

    a_elem.onclick attributes

--- codeigniter/application/helpers/common_helper.php
+++ codeigniter/application/helpers/common_helper.php
@@ -32,3 +32,9 @@ function pop_flash_message($instance)
     $str .= $instance->config->item('success_suffix');
     return $str;
 }
+
+
+function json_safe_encode($data)
+{
+    return json_encode($data, JSON_HEX_TAG | JSON_HEX_AMP | JSON_HEX_APOS | JSON_HEX_QUOT);
+}
--- codeigniter/js/common.js
+++ codeigniter/js/common.js
@@ -1,5 +1,6 @@
-function append_project_tag(e, appended_project_tag)
+function append_project_tag(e, appended_project_tag, $project_tags)
 {
+    alert($project_tags);
     // dropdown
     var parent_elem = e.parentElement;
     while (parent_elem.className != "project_tag_board")
@@ -18,9 +19,15 @@ function append_project_tag(e, appended_project_tag)
     p_elem.appendChild(select_elem);
 
     // image
+    a_elem = document.createElement('a');
+    a_elem.setAttribute('href', '#');
+    a_elem.onclick = new Function('append_project_tag(this, ' + String(appended_project_tag + 1) + ')');
+    alert(appended_project_tag);
+
     img_elem = document.createElement('img');
     img_elem.setAttribute('src', '/images/plus.png');
-    p_elem.appendChild(img_elem);
+    a_elem.appendChild(img_elem);
+    p_elem.appendChild(a_elem);
 
     parent_elem.appendChild(p_elem);    
 }
