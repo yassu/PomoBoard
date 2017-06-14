7ed7e95830c7c6044bfe63a479ab9737da669919
yassu <mathyassu@gmail.com>
2017-06-14 14:59:19 +0900

    add simple controller for TaskTag.explore

---
+++ codeigniter/application/controllers/Task_Tag.php
@@ -0,0 +1,12 @@
+<?php
+class Task_Tag extends CI_Controller {
+
+    public function explore()
+    {
+        $header_data['page_title'] = 'TaskTagExplore | PomoBoard';
+        $header_data['headline'] = 'TaskTag Explore';
+
+        $this->load->view('statics/header', $header_data);
+        $this->load->view('statics/footer');
+    }
+}
