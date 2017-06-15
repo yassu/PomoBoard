3213f192c2fcc4af7b45a352bb5ec8b7f0992283
yassu <mathyassu@gmail.com>
2017-06-15 14:34:44 +0900

    IndicatedPoint.get_list

--- codeigniter/application/controllers/Indicated_Point.php
+++ codeigniter/application/controllers/Indicated_Point.php
@@ -7,6 +7,22 @@ class Indicated_Point extends CI_Controller
         $header_data['page_title'] = 'IndicatedPoint | Pomoboard';
         $header_data['headline'] = 'Explore Indicated Point';
 
+        // echo var_dump($_POST);
+        echo var_dump($_POST);
+        if (array_key_exists('submit', $_POST) && $_POST['submit'] === 'explore')
+        {
+            echo "get_list";
+            $list = $this->IndicatedPoint->get_list($this->User->logined(),
+                $_POST['title'], $_POST['memo'], $_POST['keyword'],
+                $_POST['begin_created_date'], $_POST['end_created_date'],
+                $_POST['begin_updated_date'], $_POST['end_updated_date']);
+        }
+        else
+        {
+            $list = array();
+        }
+        echo var_dump($list);
+
         $this->load->view('statics/header', $header_data);
         $this->load->view('indicated_point/explore');
         $this->load->view('statics/footer');
--- codeigniter/application/models/MY_IndicatedPoint.php
+++ codeigniter/application/models/MY_IndicatedPoint.php
@@ -29,4 +29,60 @@ class MY_IndicatedPoint extends CI_Model
             )
         );
     }
+
+
+    public function get_list($user_id, $title, $memo, $keyword,
+        $begin_created_date, $end_created_date,
+        $begin_updated_date, $end_updated_date)
+    {
+        if (! $user_id)
+        {
+            return array();
+        }
+
+        $this->db
+            ->where('user_id', $user_id)
+            ->where('is_deleted', 0);
+
+        if ($title !== "")
+        {
+            $this->db->like('title', $title);
+        }
+
+        if ($memo !== "")
+        {
+            $this->db->like('memo', $memo);
+        }
+
+        if ($keyword !== "")
+        {
+            $this->db
+                ->group_start()
+                ->like('title', $keyword)
+                ->or_like('memo', $keyword)
+                ->group_end();
+        }
+
+        if ($begin_created_date !== "")
+        {
+            $this->db->where('created_date >= ', $begin_created_date);
+        }
+
+        if ($end_created_date !== "")
+        {
+            $this->db->where('created_date <= ', get_next_date_str($end_created_date));
+        }
+
+        if ($begin_updated_date !== "")
+        {
+            $this->db->where('updated_date >= ', $begin_updated_date);
+        }
+
+        if ($end_updated_date !== "")
+        {
+            $this->db->where('updated_date < ', get_next_date_str($end_updated_date));
+        }
+
+        return $this->db->get('IndicatedPoint')->result_array();
+    }
 }
