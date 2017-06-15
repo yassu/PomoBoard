c0bb7bec21b9cb498872ab8eb0dbcb946bd0f662
yassu <mathyassu@gmail.com>
2017-06-15 15:12:51 +0900

    indicated_point/edit/{id}でtitle, memo, formのaction先を正しくする

--- codeigniter/application/views/indicated_point/Edit.php
+++ codeigniter/application/views/indicated_point/Edit.php
@@ -1,17 +1,19 @@
-<?php echo form_open("indicated_point/edit/new"); ?>
+<?php echo form_open("indicated_point/edit/" . (($indicated_point_id === "new")? 'new': $indicated_point_id)); ?>
     <fieldset>
         <table>
             <tbody>
                 <tr>
                     <th style="text-align: left"> Indicated Point Title </th>
                     <td>
-                        <input type="text" id="indicated_point_title" name="indicated_point_title" value="">
+                        <input type="text" id="indicated_point_title" name="indicated_point_title"
+                            value="<?php echo is_null($indicated_point)? '': $indicated_point['title']; ?>">
                     </td>
                 </tr>
                 <tr>
                     <th style="text-align: left"> Indicated Point Memo </th>
                     <td>
-                        <input type="text" id="indicated_point_memo" name="indicated_point_memo" value="">
+                        <input type="text" id="indicated_point_memo" name="indicated_point_memo"
+                            value="<?php echo is_null($indicated_point)? '': $indicated_point['memo']; ?>">
                     </td>
                 </tr>
             </tbody>
