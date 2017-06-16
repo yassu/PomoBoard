718fc3bdf7fa1913af0f90f466eeb643408523c8
yassu <mathyassu@gmail.com>
2017-06-16 11:14:01 +0900

    Modelができればinsertできるように

--- codeigniter/application/controllers/Indicated_Point_Tag.php
+++ codeigniter/application/controllers/Indicated_Point_Tag.php
@@ -15,11 +15,22 @@ class Indicated_Point_Tag extends CI_Controller
 
     public function edit()
     {
+        $this->form_validation->set_rules('indicated_point_tag_name', 'Indicated Point Tag name', 'trim|required');
+
         $header_data['page_title'] = 'New IndicatedPointTag | PomoBoard';
         $header_data['headline'] = 'New Indicated Point Tag';
 
-        $this->load->view('statics/header', $header_data);
-        $this->load->view('indicated_point_tag/edit');
-        $this->load->view('statics/footer');
+        if ($this->form_validation->run() === false)
+        {
+            $this->load->view('statics/header', $header_data);
+            $this->load->view('indicated_point_tag/edit');
+            $this->load->view('statics/footer');
+        }
+        else
+        {
+            $this->IndicatedPointTag->insert($this->User->logined(), $_POST['indicated_point_tag_name']);
+            set_flash_message($this, 'Inserted new indicated point tag');
+            redirect('indicated_point_tag/explore');
+        }
     }
 }
