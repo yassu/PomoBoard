275966e55696c26eec8f3788d256c3bd2cbfaa0f
yassu <mathyassu@gmail.com>
2017-06-16 11:06:35 +0900

    append new point tag/edit viewer

--- codeigniter/application/controllers/Indicated_Point_Tag.php
+++ codeigniter/application/controllers/Indicated_Point_Tag.php
@@ -11,4 +11,15 @@ class Indicated_Point_Tag extends CI_Controller
         $this->load->view('indicated_point_tag/explore');
         $this->load->view('statics/footer');
     }
+
+
+    public function edit()
+    {
+        $header_data['page_title'] = 'New IndicatedPointTag | PomoBoard';
+        $header_data['headline'] = 'New Indicated Point Tag';
+
+        $this->load->view('statics/header', $header_data);
+        $this->load->view('indicated_point_tag/edit');
+        $this->load->view('statics/footer');
+    }
 }
---
+++ codeigniter/application/views/indicated_point_tag/edit.php
@@ -0,0 +1,14 @@
+<?php echo form_open("indicated_point_tag/edit/new"); ?>
+    <fieldset>
+        <table>
+            <tbody>
+                <tr>
+                    <th style='text-align: left'>Indicated Point Tag Name</th>
+                        <td><input type="text" id="indicated_point_tag_name" name="indicated_point_tag_name"
+                            value="" /></td>
+                </tr>
+            </tbody>
+        </table>
+    </fieldset>
+    <p class="submit"><input type="submit" value="submit"/></p>
+</form>
